8c982c0e109b20d11772aae66cf675f71c9e9654
Taylor Stallings <tstallings@apptio.com>
2023-02-14 18:22:31 -0500

    getting post route to update db file

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const uuid = require('./helpers/uuid');
 // initialize app
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // get route for notes.html file
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname,'/public/notes.html'))
@@ -18,11 +22,29 @@ app.get('/notes', (req, res) =>
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
 // api get route to read db.json file
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+// defining readAndAppend
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 // post route for new note
 app.post('/api/notes', (req, res) => {
